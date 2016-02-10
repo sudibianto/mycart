40d884d6f6464b55b096652179e67e6e0287d980
sudibianto <sudibianto@gmail.com>
2016-02-10 12:17:25 +0700

    new commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://mycart.localhost/admin/');
+define('HTTP_CATALOG', 'http://mycart.localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://mycart.localhost/admin/');
+define('HTTPS_CATALOG', 'http://mycart.localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/Apache24/htdocs/mycart/admin/');
+define('DIR_SYSTEM', 'D:/Apache24/htdocs/mycart/system/');
+define('DIR_LANGUAGE', 'D:/Apache24/htdocs/mycart/admin/language/');
+define('DIR_TEMPLATE', 'D:/Apache24/htdocs/mycart/admin/view/template/');
+define('DIR_CONFIG', 'D:/Apache24/htdocs/mycart/system/config/');
+define('DIR_IMAGE', 'D:/Apache24/htdocs/mycart/image/');
+define('DIR_CACHE', 'D:/Apache24/htdocs/mycart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/Apache24/htdocs/mycart/system/storage/download/');
+define('DIR_LOGS', 'D:/Apache24/htdocs/mycart/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/Apache24/htdocs/mycart/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/Apache24/htdocs/mycart/system/storage/upload/');
+define('DIR_CATALOG', 'D:/Apache24/htdocs/mycart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'asdqwe1234');
+define('DB_DATABASE', 'mycart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://mycart.localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://mycart.localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/Apache24/htdocs/mycart/catalog/');
+define('DIR_SYSTEM', 'D:/Apache24/htdocs/mycart/system/');
+define('DIR_LANGUAGE', 'D:/Apache24/htdocs/mycart/catalog/language/');
+define('DIR_TEMPLATE', 'D:/Apache24/htdocs/mycart/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/Apache24/htdocs/mycart/system/config/');
+define('DIR_IMAGE', 'D:/Apache24/htdocs/mycart/image/');
+define('DIR_CACHE', 'D:/Apache24/htdocs/mycart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/Apache24/htdocs/mycart/system/storage/download/');
+define('DIR_LOGS', 'D:/Apache24/htdocs/mycart/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/Apache24/htdocs/mycart/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/Apache24/htdocs/mycart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'asdqwe1234');
+define('DB_DATABASE', 'mycart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
